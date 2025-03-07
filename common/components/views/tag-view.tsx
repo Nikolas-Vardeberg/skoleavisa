f2f6ef4a7db5ccd3267ec3826577200328cfbee8
niklusrook <nikolas.vardeberg1@gmail.com>
2025-03-07 10:27:25 +0100

    chore: update next

--- common/components/views/tag-view.tsx
+++ common/components/views/tag-view.tsx
@@ -7,13 +7,15 @@ import RichText from "../RichText";
 
 export default function TagView({ data }: { data: Tags }) {
     return(
-        <div className="flex flex-col gap-5 max-w-[1200px] mx-auto py-10 px-8">
-            <h1 className="text-3xl sm:text-5xl">{data.title}</h1>
-            <RichText blocks={data.description ?? []} elementClassName="lead"  />
-            <div className="grid grid-cols-1 md:grid-cols-2 justify-between gap-8">
-                {data.related?.map((page: Page) => (
-                    <TransportItem page={page} key={page._id} />
-                ))}
+        <div className="bg-blue-200">
+            <div className="flex flex-col gap-5 max-w-[1200px] mx-auto py-10 px-8">
+                <h1 className="text-3xl sm:text-5xl">{data.title}</h1>
+                <RichText blocks={data.description ?? []} elementClassName="lead"  />
+                <div className="grid grid-cols-1 md:grid-cols-2 justify-between gap-8">
+                    {data.related?.map((page: Page) => (
+                        <TransportItem page={page} key={page._id} />
+                    ))}
+                </div>
             </div>
         </div>
     )
